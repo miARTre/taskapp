907e2061ae4f4aafee433576618d86e012638a6d
Mirnes Becirbegovic <mirnesbb@gmail.com>
2025-07-09 15:48:47 +0200

    Save all progress so far in index-testing.js

---
+++ src/index-testing.js
@@ -0,0 +1,131 @@
+const express = require("express");
+require("./db/mongoose");
+const User = require("./models/user");
+const Task = require("./models/task");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post("/users", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+
+  // user
+  //   .save()
+  //   .then((user) => {
+  //     res.status(201).send(user);
+  //   })
+  //   .catch((e) => {
+  //     res.status(400).send(e);
+  //   });
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+
+  // User.find({})
+  //   .then((users) => {
+  //     res.send(users);
+  //   })
+  //   .catch((e) => {
+  //     res.status(500);
+  //   });
+});
+
+app.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const userById = await User.findById(_id);
+    if (!userById) {
+      return res.status(404).send();
+    }
+    res.send(userById);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+
+  //   console.log(req.params);
+  //   console.log(req.params.id);
+  // const _id = req.params.id;
+
+  // User.findById(_id)
+  //   .then((user) => {
+  //     if (!user) {
+  //       return res.status(404).send();
+  //     }
+  //     res.send(user);
+  //   })
+  //   .catch((e) => {
+  //     res.status(500).send();
+  //   });
+});
+
+app.post("/tasks", async (req, res) => {
+  const task = new Task(req.body);
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+
+  // task
+  //   .save()
+  //   .then((task) => res.status(201).send(task))
+  //   .catch((e) => res.status(400).send(e));
+});
+
+app.get("/tasks", async (req, res) => {
+  try {
+    const task = await Task.find({});
+    res.send(task);
+  } catch (e) {
+    res.send(500).send(e);
+  }
+
+  // Task.find({})
+  //   .then((tasks) => res.status(200).send(tasks))
+  //   .catch((e) => res.status(500).send());
+});
+
+app.get("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+
+  // Task.findById(_id)
+  //   .then((task) => {
+  //     if (!task) {
+  //       return res.status(404).send();
+  //     }
+  //     res.send(task);
+  //   })
+  //   .catch((e) => res.status(500).send());
+});
+
+app.listen(PORT, () => {
+  console.log("Server run on PORT:", PORT);
+});
