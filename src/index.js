df7c4076b86ec9a8a576da4b99c495ba10d040b1
Mirnes Becirbegovic <mirnesbb@gmail.com>
2025-07-09 12:59:15 +0200

    Add GET /users, /users/:id, /tasks, and /tasks/:id endpoints for retrieving users and tasks

--- src/index.js
+++ src/index.js
@@ -22,6 +22,33 @@ app.post("/users", (req, res) => {
     });
 });
 
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((e) => {
+      res.status(500);
+    });
+}); 
+
+app.get("/users/:id", (req, res) => {
+  //   console.log(req.params);
+  //   console.log(req.params.id);
+  const _id = req.params.id;
+
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
 app.post("/tasks", (req, res) => {
   const task = new Task(req.body);
 
@@ -29,6 +56,25 @@ app.post("/tasks", (req, res) => {
     .save()
     .then((task) => res.status(201).send(task))
     .catch((e) => res.status(400).send(e));
+});
+
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => res.status(200).send(tasks))
+    .catch((e) => res.status(500).send());
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+      res.send(task);
+    })
+    .catch((e) => res.status(500).send());
 });
 
 app.listen(PORT, () => {
