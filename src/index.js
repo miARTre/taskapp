0aabd08fbcc3381816684ca49bc0115335d463c5
Mirnes Becirbegovic <mirnesbb@gmail.com>
2025-07-10 11:12:57 +0200

    feat: add static method to User model for credential verification

--- src/index.js
+++ src/index.js
@@ -14,3 +14,18 @@ app.use(taskRouter);
 app.listen(PORT, () => {
   console.log("Server run on PORT:", PORT);
 });
+
+const bcrypt = require("bcryptjs");
+
+const myFun = async () => {
+  const password = "Red1234!";
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  console.log(password);
+  console.log(hashedPassword);
+
+  const isMatch = await bcrypt.compare('red1234!', hashedPassword);
+  console.log(isMatch)
+};
+
+myFun();
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const User = require('../models/user');
+const express = require("express");
+const User = require("../models/user");
 
 const router = express.Router();
 
@@ -11,6 +11,18 @@ router.post("/users", async (req, res) => {
     res.status(201).send(user);
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByIdCredentials(
+      req.body.email,
+      req.body.password
+    );
+    res.send(user);
+  } catch (e) {
+    res.status(401).send(e);
   }
 });
 
@@ -49,10 +61,17 @@ router.patch("/users/:id", async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    // const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    //   new: true,
+    //   runValidators: true,
+    // });
+
+    const user = await User.findById(req.params.id);
+
+    updates.forEach((update) => (user[update] = req.body[update]));
+
+    await user.save();
+
     if (!user) {
       return res.status(404).send();
     }
