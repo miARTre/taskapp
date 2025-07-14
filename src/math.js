7d11f070aa3029d6a20029959c9ca25aafab3950
Mirnes Becirbegovic <mirnesbb@gmail.com>
2025-07-14 15:06:19 +0200

    Add tip and temperature conversion utilities with Jest tests

---
+++ src/math.js
@@ -0,0 +1,20 @@
+const calculateTip = (total, tipPrecent = 0.25) => total + total * tipPrecent;
+//{
+//const tip = total * tipPrecent
+//return total + tip;
+//};
+
+
+const fahrenheitToCelsius = (temp) => {
+    return (temp - 32) / 1.8
+}
+
+const celsiusToFahrenheit = (temp) => {
+    return (temp * 1.8) + 32
+}
+
+module.exports = {
+  calculateTip,
+  fahrenheitToCelsius,
+  celsiusToFahrenheit
+};
