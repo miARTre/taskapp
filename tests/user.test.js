696ecc21d5b4832caa07de3a8209bd547a680b27
Mirnes Becirbegovic <mirnesbb@gmail.com>
2025-07-15 11:01:30 +0200

    test: verify user signup, login success and failure with wrong credentials

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,46 @@
-require("dotenv").config({ path: "./src/config/.env" });
+require("dotenv").config({ path: "./src/config/test.env" });
+const User = require("../src/models/user");
 const request = require("supertest");
 const app = require("../src/app");
 
+const UserOne = {
+  name: "Test User",
+  email: "test@test.com",
+  password: "12345what",
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(UserOne).save();
+});
+
 test("should first", async () => {
-  await request(app).post("/users").send({
-    name: "Mire Test",
-    email: "mirnesbb@gmail.com",
-    password: "MyPass777!",
-  }).expect(201);
+  await request(app)
+    .post("/users")
+    .send({
+      name: "Mire Test",
+      email: "mirnesbb@gmail.com",
+      password: "MyPass777!",
+    })
+    .expect(201);
+});
+
+test("should login existing user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: "test@test.com",
+      password: "12345what",
+    })
+    .expect(200);
+});
+
+test("should return error for wrong login credentials", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: UserOne.email,
+      password: "randomPass",
+    })
+    .expect(401);
 });
