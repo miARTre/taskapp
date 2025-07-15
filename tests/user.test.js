1be4016a0b95b1fad9a832b5d6a6144c1c8f712e
Mirnes Becirbegovic <mirnesbb@gmail.com>
2025-07-15 11:47:18 +0200

    test: implement authenticated and unauthenticated tests for profile retrieval and user deletion

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,17 +1,31 @@
 require("dotenv").config({ path: "./src/config/test.env" });
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const User = require("../src/models/user");
 const request = require("supertest");
 const app = require("../src/app");
+jest.mock("@sendgrid/mail", () => ({
+  setApiKey: jest.fn(),
+  send: jest.fn().mockResolvedValue([{ statusCode: 202 }]),
+}));
 
-const UserOne = {
+const userOneId = new mongoose.Types.ObjectId();
+
+const userOne = {
+  _id: userOneId,
   name: "Test User",
   email: "test@test.com",
   password: "12345what",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
+    },
+  ],
 };
 
 beforeEach(async () => {
   await User.deleteMany();
-  await new User(UserOne).save();
+  await new User(userOne).save();
 });
 
 test("should first", async () => {
@@ -39,8 +53,32 @@ test("should return error for wrong login credentials", async () => {
   await request(app)
     .post("/users/login")
     .send({
-      email: UserOne.email,
+      email: userOne.email,
       password: "randomPass",
     })
     .expect(401);
+});
+
+test("should get profile for user", async () => {
+  await request(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("should not get profile for unauthenticated user", async () => {
+  await request(app).get("/users/me").send().expect(401);
+});
+
+test("should delete account for user", async () => {
+  await request(app)
+    .delete("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("should not delete account for user", async () => {
+  await request(app).delete("/users/me").send().expect(401);
 });
